ff7d155af48092c78eed8bd6392f4d3d882a559d
Eirik S. Morland <eirik@morland.no>
2024-10-14 08:42:01 +0200

    Show its loading

--- worker.mjs
+++ worker.mjs
@@ -7,7 +7,7 @@ export function setUpWorker(worker) {
         if (url.pathname.substr(0, prefix.length) === prefix && url.hostname === self.location.hostname) {
             event.respondWith(
                 (async () => {
-                  return new Response(`<html><body class="sw-body"><h1>200</h1>${url} found</body></html>`, {
+                  return new Response(`<html><body class="sw-body"><h1>200</h1>${url} found<script>window.onload = function() { let p = document.createElement('p');p.innerHTML = 'this is written with document write on load';document.querySelector('body').append(p) }</script></body></html>`, {
                     status: 200,
                     headers: {"Content-Type": "text/html"},
                   })
